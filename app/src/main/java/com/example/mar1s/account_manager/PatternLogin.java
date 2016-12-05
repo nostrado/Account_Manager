eed8cd1d643c821f5dd25aa948cd02d593cc5c05
mar1s <mar1st1145@gmail.com>
2016-12-05 22:09:59 +0900

    commit

---
+++ app/src/main/java/com/example/mar1s/account_manager/PatternLogin.java
@@ -0,0 +1,13 @@
+package com.example.mar1s.account_manager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PatternLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pattern_login);
+    }
+}
