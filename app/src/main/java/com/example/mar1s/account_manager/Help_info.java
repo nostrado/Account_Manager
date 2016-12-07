d23447db891aaaae97a16251a03d552c21a56a1c
mar1s <mar1st1145@gmail.com>
2016-12-07 22:28:06 +0900

    commit new files

---
+++ app/src/main/java/com/example/mar1s/account_manager/Help_info.java
@@ -0,0 +1,17 @@
+package com.example.mar1s.account_manager;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by mar1s on 2016-12-07.
+ */
+
+public class Help_info extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help_info);
+    }
+}
